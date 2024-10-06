a70d7a63767a26d31855510a881eced3d0e33aec
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2024-10-07 01:40:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,142 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// Middle Ware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@master-job-shop.46woq.mongodb.net/?retryWrites=true&w=majority&appName=Master-Job-Shop`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Connection
+    const PostedJobCollection = client
+      .db("Master-Job-Shop")
+      .collection("Posted-Job");
+    const PostedGigCollection = client
+      .db("Master-Job-Shop")
+      .collection("Posted-Gig");
+    const CompanyProfilesCollection = client
+      .db("Master-Job-Shop")
+      .collection("Company-Profiles");
+    const SalaryInsightCollection = client
+      .db("Master-Job-Shop")
+      .collection("Salary-Insight");
+    const UpcomingEventsCollection = client
+      .db("Master-Job-Shop")
+      .collection("Upcoming-Events");
+
+    //API`s
+    // Posted Job API
+    // Get Posted Job
+    app.get("/Posted-Job", async (req, res) => {
+      const result = await PostedJobCollection.find().toArray();
+      res.send(result);
+    });
+    // get Posed Posted Job by ID
+    app.get("/Posted-Job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await PostedJobCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Posted Gig API
+    // Get Posted Gig
+    app.get("/Posted-Gig", async (req, res) => {
+      const result = await PostedGigCollection.find().toArray();
+      res.send(result);
+    });
+    // get Posed Posted Gig by ID
+    app.get("/Posted-Gig/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await PostedGigCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Company Profiles API
+    // Get Company Profiles
+    app.get("/Company-Profiles", async (req, res) => {
+      const result = await CompanyProfilesCollection.find().toArray();
+      res.send(result);
+    });
+    // get Posed Company Profiles by ID
+    app.get("/Company-Profiles/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await CompanyProfilesCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Salary Insight API
+    // Get Salary Insight
+    app.get("/Salary-Insight", async (req, res) => {
+      const result = await SalaryInsightCollection.find().toArray();
+      res.send(result);
+    });
+    // get Posed Salary Insight by ID
+    app.get("/Salary-Insight/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await SalaryInsightCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Upcoming Events API
+    // Get Upcoming Events
+    app.get("/Upcoming-Events", async (req, res) => {
+      const result = await UpcomingEventsCollection.find().toArray();
+      res.send(result);
+    });
+    // get Posed Upcoming Events by ID
+    app.get("/Upcoming-Events/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await UpcomingEventsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Set up the basic route
+app.get("/", (req, res) => {
+  res.send("Master Job Shop is Running");
+});
+
+// Listen on the specified port
+app.listen(port, () => {
+  console.log(`Master Job Shop is Running on Port: ${port}`);
+});

7498f05ed12ab7822deeecc73912fd9ae1035228
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2025-07-03 07:30:14 +0600

    Refactor database connection paths in route files and add MongoDB connection setup

--- routes/HomeBanner/HomeBanner.js
+++ routes/HomeBanner/HomeBanner.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { ObjectId } = require("mongodb");
-const { client } = require("../config/db");
+const { client } = require("../../config/db");
 
 const HomeBannerCollection = client
   .db("Master-Job-Shop")

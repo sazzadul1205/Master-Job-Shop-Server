330305336d10d47fbe68081c9d096bde457344bd
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2025-07-31 20:59:52 +0600

    - Added /SoftDelete/:id that will allow me to Delete the Users Data

--- routes/User/User.js
+++ routes/User/User.js
@@ -544,4 +544,45 @@ router.delete("/DeleteSkill/:id", async (req, res) => {
   }
 });
 
+// DELETE Soft delete a user by ID
+router.delete("/SoftDelete/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    if (!ObjectId.isValid(userId)) {
+      return res.status(400).json({ message: "Invalid user ID." });
+    }
+
+    const user = await UsersCollection.findOne({ _id: new ObjectId(userId) });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const fieldsToPreserve = ["_id", "email"];
+    const fieldsToUnset = {};
+
+    Object.keys(user).forEach((key) => {
+      if (!fieldsToPreserve.includes(key)) {
+        fieldsToUnset[key] = "";
+      }
+    });
+
+    const updateOps = {
+      $unset: fieldsToUnset,
+      $set: {
+        deleteStatus: true,
+        deletedAt: new Date(),
+      },
+    };
+
+    await UsersCollection.updateOne({ _id: new ObjectId(userId) }, updateOps);
+
+    res.status(200).json({ message: "User soft-deleted successfully." });
+  } catch (error) {
+    console.error("DELETE /users/soft-delete/:id error:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
+
 module.exports = router;
